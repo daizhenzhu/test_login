2176f74f06e6020df7a43cc51838a2c60a500157
zhulin <1498565690@qq.com>
2018-03-30 20:59:54 +0800

    hushuhua

---
+++ scripts/test_login.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+class Test_Allure:
+
+    def test_allure1(self):
+        assert 1
+
+    def test_allure2(self):
+        assert 1
+
+    def test_allure3(self):
+        assert 1
